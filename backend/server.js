d5efd6955a555b5e10594f3170f123751548bf61
Mohamed Ibrahim <mohdayman22rr@gmail.com>
2023-11-13 09:22:43 +0300

    initial

--- backend/server.js
+++ backend/server.js
@@ -1,86 +1,120 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-async function scrapeInstagramAPKs() {
+const scrapeInstagramAPKs = async () => {
   try {
     const response = await axios.get(
       "https://www.apkmirror.com/apk/instagram/instagram-instagram/"
     );
 
     const versions = [];
-    const limit = 10; // Set the desired limit
+    const limit = 1; // Set the desired limit
     const $ = cheerio.load(response.data);
 
     // Iterate over individual APK rows
-    $(".appRow .table-row").each((index, element) => {
+    $(".appRow .table-row").each(async (index, e) => {
       if (index < limit) {
         // Extract the version information
-        const versionInfo = $(element)
+        const versionInfo = $(e)
           .find(".appRowTitle.wrapText.marginZero.block-on-mobile")
           .text()
           .trim();
 
         // Extract the release date
-        const versionReleaseDate = $(element)
+        const versionReleaseDate = $(e)
           .find(".dateyear_utc")
-          .text()
+          .attr("data-utcdate")
           .trim();
 
+        const variantsCount = $(e).find(".appRowVariantTag").text().trim();
+        const variantsURL = $(e)
+          .find(".appRowVariantTag a")
+          .attr("href")
+          .trim();
+        const variants = await scrapeVariants(
+          `https://www.apkmirror.com${variantsURL}`,
+          versionInfo
+        );
+        console.log(variants);
         // Push the version information and release date as an object to the array
-        versions.push({
-          version: versionInfo,
-          releaseDate: versionReleaseDate,
-        });
+        // versions.push({
+        //   version: versionInfo,
+        //   releaseDate: versionReleaseDate,
+        //   variantsCount,
+        //   variantsURL,
+        //   variants,
+        // });
       }
     });
 
     // Log the array of version information
     console.log(versions);
   } catch (error) {
-    console.error("Error during scraping:", error.message);
+    console.error("Error during scraping versions:", error.message);
   }
-}
+};
+
+const scrapeVariants = async (url, versionId) => {
+  try {
+    const response = await axios.get(url);
+
+    const variants = [];
+    const $ = cheerio.load(response.data);
+
+    // Iterate over individual APK rows
+    $(".variants-table .table-row:not(:first-child)").each((index, e) => {
+      // Extract the variant information
+      // console.log($(e).text());
+      const variantId = $(e)
+        .find(".table-cell:eq(0) .colorLightBlack:eq(0)")
+        .text()
+        .trim();
+
+      // Extract the architecture
+      const variantArchitecture = $(e).find(".table-cell:eq(1)").text().trim();
+
+      const variantMinAndroidVersion = $(e)
+        .find(".table-cell:eq(2)")
+        .text()
+        .trim();
+      const variantDpi = $(e).find(".table-cell:eq(3)").text().trim();
+      // Push the variant information as an object to the array
+      variants.push({
+        versionId: versionId,
+        variantId,
+        variantArchitecture: variantArchitecture,
+        variantMinAndroidVersion: variantMinAndroidVersion,
+        dpi: variantDpi,
+      });
+    });
+
+    // Log the array of version information
+    return variants;
+  } catch (error) {
+    console.error("Error during scraping variants:", error.message);
+  }
+};
 
 // Uncomment the line below to trigger the function
 scrapeInstagramAPKs();
+// scrapeVariants(
+//   "https://www.apkmirror.com/apk/instagram/instagram-instagram/instagram-instagram-309-0-0-30-113-release/"
+// );
+// app.js
 
-// const axios = require("axios");
-// const cheerio = require("cheerio");
+// const express = require('express');
+// const mongoose = require('mongoose');
+// const apiRoutes = require('./routes/apiRoutes');
+// const fetchRoute = require('./routes/fetchRoute');
 
-// let versions = [];
-// async function scrapeInstagramAPKs() {
-//   try {
-//     const response = await axios.get(
-//       "https://www.apkmirror.com/apk/instagram/instagram-instagram/"
-//     );
+// const app = express();
+// const port = 3000;
 
-//     const $ = cheerio.load(response.data);
-//     let x = $("#primary > div.listWidget.p-relative").text();
-//     // Assuming you want to extract information about each APK variant
-//     $("#primary > div.listWidget.p-relative").each((index, element) => {
-//       const rawText = $(element)
-//         .find(".appRowTitle.wrapText.marginZero.block-on-mobile")
-//         .text()
-//         .trim();
-//       const versionInfoArray = rawText.split("\n").map((item) => item.trim());
+// mongoose.connect('mongodb://localhost/instagramApp', { useNewUrlParser: true, useUnifiedTopology: true });
 
-//       // Extract the release date
-//       const versionReleaseDate = $(element)
-//         .find(".dateyear_utc")
-//         .first() // Use first() to get only the first occurrence
-//         .text()
-//         .trim();
+// app.use('/api', apiRoutes);
+// app.use('/fetch', fetchRoute);
 
-//       // Process each version separately
-//       versionInfoArray.forEach((version) => {
-//         versions.push({ version, releaseDate: versionReleaseDate });
-//       });
-//     });
-//     console.log(versions[0]);
-//   } catch (error) {
-//     console.error("Error during scraping:", error.message);
-//   }
-// }
-
-// // Uncomment the line below to trigger the function
-// scrapeInstagramAPKs();
+// app.listen(port, () => {
+//   console.log(`App running on http://localhost:${port}`);
+// });
