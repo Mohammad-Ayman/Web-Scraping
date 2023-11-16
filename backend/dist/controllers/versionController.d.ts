28f4e549d38d00ca8f9708555a9deef655f71a63
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-11-16 11:04:04 +0300

    Delete backend/dist directory

--- backend/dist/controllers/versionController.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from "express";
-declare const versionController: {
-    getAllVersions: (req: Request, res: Response) => Promise<void>;
-    getVersionById: (req: Request, res: Response) => Promise<void>;
-    deleteVersionById: (req: Request, res: Response) => Promise<void>;
-    updateVersionById: (req: Request, res: Response) => Promise<void>;
-};
-export default versionController;
--- backend/dist/db/connection.d.ts
+++
@@ -1,5 +0,0 @@
-import "dotenv/config";
-import mongoose from "mongoose";
-export declare const connectToMongo: () => Promise<void>;
-export declare const clearDatabase: () => Promise<void>;
-export default mongoose;
