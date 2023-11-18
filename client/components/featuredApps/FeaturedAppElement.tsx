c17edaaa7eb8fc4fc6c2e4bae43f4e2326cf5c12
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-11-18 21:15:30 +0300

    ENHANCE: hover over delete

--- client/components/featuredApps/FeaturedAppElement.tsx
+++ client/components/featuredApps/FeaturedAppElement.tsx
@@ -1,51 +1,18 @@
 "use client";
 import appContext from "@/store/App-Context";
 import styles from "./styles/featuredAppElement.module.css";
-import { styled } from "styled-components";
 import Image from "next/image";
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 
-const PriceBox = styled.span`
-  background-color: #2e8dff;
-  color: #fbfbfb;
-  padding: 0.4rem 1.2rem;
-  border-radius: 1rem;
-`;
-
-const ButtonContainer = styled.div`
-  position: relative;
-  color: #333;
-`;
-
-const BookmarkButton = styled.button`
-  background: rgba(128, 128, 128, 0.5);
-  border: none;
-  border-radius: 0.6rem;
-  padding: 0.6rem;
-  position: absolute;
-  top: -18rem;
-  right: 0.5rem;
-
-  .bookmark-icon {
-    color: #ffffff;
-    width: 1.5rem;
-    height: 1.5rem;
-  }
-  &:hover {
-    transform: scale(1.15);
-  }
-`;
 interface FeaturedAppElementProps {
   id: string;
   name: string;
   image: string;
   url: string;
-  // totalVariants: number;
 }
 const FeaturedAppElement: React.FC<FeaturedAppElementProps> = (props) => {
   let appCtx = useContext(appContext);
   const updateDisplayedApp = () => {
-    // console.log("Clicked", props.id);
     appCtx?.setDisplayedApp(props.id);
     appCtx?.setAppUrl(props.url);
     console.log(appCtx?.displayedApp);
--- client/components/featuredApps/FeaturedApps.tsx
+++ client/components/featuredApps/FeaturedApps.tsx
@@ -1,12 +1,7 @@
-"use client";
-import React, { useState, useEffect } from "react";
 import DisplayApps from "./DisplayApps";
 import styles from "./styles/featuredApps.module.css";
 
 const FeaturedApps = () => {
-  const [versions, setVersions] = useState([]);
-  const [featuredApp, setFeaturedApp] = useState("");
-  useEffect(() => {}, []);
   return (
     <section className={styles["features-container"]}>
       <h2 className="header-text">Featured Apps</h2>
