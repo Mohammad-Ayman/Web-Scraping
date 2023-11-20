056650f73d81fba1f8f5f6a8b85bcb1379e0a084
Mohamed Ayman <mohdayman22rr@gmail.com>
2023-11-20 15:19:27 +0300

    Update port Number

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import apiRoutes from "./routes/apiRoutes.js";
 import fetchRoute from "./routes/fetchRoute.js";
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 
